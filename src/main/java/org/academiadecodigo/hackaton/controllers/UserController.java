827e473e226265012d9848722f643580c7cbd278
Pedro Silva <pedroheenriquee369@gmail.com>
2021-08-05 21:43:45 +0100

    Controlers

--- src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
+++ src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
@@ -6,23 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @Autowired
     private UserRepository userRepository;
 
-
-    @GetMapping("/getUser")
-    private ResponseEntity<User> getUSer(){
-
-        User user = new User();
-        user.setName("J");
-
-        userRepository.save(user);
-
-        return new ResponseEntity<>(userRepository.getById(1), HttpStatus.OK);
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
+    @GetMapping("/users")
+    private ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/add")
+    private ResponseEntity<List<User>> addUsers(){
+
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    private ResponseEntity<?> getUser(@PathVariable Integer id){
+
+        User user = userRepository.getById(id);
+
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+
+
 }
