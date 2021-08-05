b2523de1f64cfc969936889a31f2ff945ce2e30f
joao borges <joaoborges@gmail.com>
2021-08-05 22:13:13 +0100

    converters/controllers

--- src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
+++ src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
@@ -2,21 +2,42 @@ package org.academiadecodigo.hackaton.controllers;
 
 import org.academiadecodigo.hackaton.models.User;
 import org.academiadecodigo.hackaton.repositorys.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+
 @RestController
 public class UserController {
 
+
+    // ModelMapper
     @Autowired
     private UserRepository userRepository;
 
+    // ModelMapper
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // This Method will convert our object into User
+    private User toUser(User user) {
+        return modelMapper.map(user, User.class);
+    }
+
+    @GetMapping
+    private ResponseEntity<User> findAllUsers() {
+        return (ResponseEntity<User>) userRepository.findAll()
+                .stream()
+                .map(this::toUser);
+
+    }
 
     @GetMapping("/getUser")
-    private ResponseEntity<User> getUSer(){
+     private ResponseEntity<User> getUSer(){
 
         User user = new User();
         user.setName("J");
